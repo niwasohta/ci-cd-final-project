665f0ef50316f697899368c755381be1d34c38c8
niwasohta <niwasota@outlook.com>
2023-12-20 03:38:58 -0500

    refactering the code

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -47,7 +47,7 @@ def not_found(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_404_NOT_FOUND, 
+            status=status.HTTP_404_NOT_FOUND,
             error="Not Found",
             message=message),
         status.HTTP_404_NOT_FOUND,
